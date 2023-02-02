9295455c028a8741483d71e06a75a97a748a6ce2
raw <ryaboff99@gmail.com>
2023-02-02 21:55:25 +0200

    Add RecipeControllerTest

---
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -0,0 +1,47 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+public class RecipeControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    RecipeController recipeController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        recipeController = new RecipeController(recipeService);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+    }
+
+    @Test
+    void getRecipe() throws Exception {
+        Long id = 1L;
+        Recipe recipe = new Recipe();
+        recipe.setId(id);
+
+        when(recipeService.findById(id)).thenReturn(recipe);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/recipe/show/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(model().attributeExists("recipe"));
+    }
+}
