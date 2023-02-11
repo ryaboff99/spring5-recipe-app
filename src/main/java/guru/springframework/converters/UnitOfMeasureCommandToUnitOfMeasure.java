c392a7a9202c5101c6776be967aa7dda4e1b6b6f
raw <ryaboff99@gmail.com>
2023-02-12 01:00:32 +0200

    Add Type Conversions
    * Add CategoryCommandToCategory.java
    * Add CategoryCommandToCategory.java
    * Add CategoryCommandToCategoryTest.java
    * Add CategoryToCategoryCommandTest.java
    * Add IngredientCommandToIngredient.java
    * Add IngredientToIngredientCommand.java
    * Add IngredientCommandToIngredientTest.java
    * Add IngredientToIngredientCommandTest.java
    * Add NotesCommandToNotes.java
    * Add NotesCommandToNotesTest.java
    * Add NotesToNotesCommand.java
    * Add NotesToNotesCommandTest.java
    * Add RecipeCommandToRecipe.java
    * Add RecipeCommandToRecipeTest.java
    * Add RecipeToRecipeCommand.java
    * Add RecipeToRecipeCommandTest.java
    * Add UnitOfMeasureCommandToUnitOfMeasure.java
    * Add UnitOfMeasureCommandToUnitOfMeasureTest.java
    * Add UnitOfMeasureToUnitOfMeasureCommand.java
    * Add UnitOfMeasureToUnitOfMeasureCommandTest.java

---
+++ src/main/java/guru/springframework/converters/UnitOfMeasureCommandToUnitOfMeasure.java
@@ -0,0 +1,27 @@
+package guru.springframework.converters;
+
+import guru.springframework.commands.UnitOfMeasureCommand;
+import guru.springframework.domain.UnitOfMeasure;
+import lombok.Synchronized;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.core.convert.converter.Converter;
+
+@Component
+public class UnitOfMeasureCommandToUnitOfMeasure implements Converter<UnitOfMeasureCommand, UnitOfMeasure> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public UnitOfMeasure convert(UnitOfMeasureCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final UnitOfMeasure uom = new UnitOfMeasure();
+        uom.setId(source.getId());
+        uom.setDescription(source.getDescription());
+        return uom;
+    }
+
+}
